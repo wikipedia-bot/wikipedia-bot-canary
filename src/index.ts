628f700cca0b43f948e23db0aa6d72e9f2d3624f
MeerBiene <60227302+MeerBiene@users.noreply.github.com>
2021-04-11 18:50:10 +0200

    fix(*): fix tokenleak

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,9 @@ app.use(express.json());
 app.listen(3333);
 
 const creator = new SlashCreator({
-  applicationID:
-    process.env.APPLICATION_ID ?? '557923645532536852',
-  publicKey:
-    process.env.PUBLIC_KEY ??
-    '0f0866ca6ff76026770aea9a0d48699ca9312e2f0c8696632b1c938ea1757c15',
-  token:
-    process.env.TOKEN ??
-    'NTU3OTIzNjQ1NTMyNTM2ODUy.XJJE4g.idx-E2oqaYY_oAH62MetUBLXlj8',
+  applicationID: process.env.APPLICATION_ID ?? '',
+  publicKey: process.env.PUBLIC_KEY ?? '',
+  token: process.env.TOKEN ?? '',
 });
 
 creator
